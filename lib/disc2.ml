355394b413120ea3c0e8de7c7322bc77267341a6
cjpopova <casparpopova2023@u.northwestern.edu>
2025-02-07 10:18:37 -0500

    Add discussion problems and instructions

--- lib/disc2.ml
+++ lib/disc2.ml
@@ -1,1 +1,14 @@
 let abs x = if x < 0 then (-1)*x else x
+
+let rec remove_all lst x = 
+  match lst with 
+  | [] -> []
+  |  h :: t -> if h == x then remove_all t x else h :: remove_all t x
+
+
+let rec index_of_helper lst x idx = 
+  match lst with 
+  | [] -> -1
+  | h :: t -> if h == x then idx else index_of_helper t x (idx+1)
+
+let index_of lst x = index_of_helper lst x 0
--- test/test_disc2.ml
+++ test/test_disc2.ml
@@ -6,12 +6,26 @@ let test_abs _ =
   assert_equal 2 (abs 2) ~msg:"test_abs (2)";
   assert_equal 1 (abs (-1)) ~msg:"test_abs (3)";
   assert_equal 2 (abs (-2)) ~msg:"test_abs (4)";
-  assert_equal 0 (abs (0)) ~msg:"test_abs (5)"
+  assert_equal 0 (abs (0)) ~msg:"test_abs (5)" 
 
+
+let test_remove_all _ =
+  assert_equal [2;3] (remove_all [1;2;3;1] 1 ) ~msg:"test_remove_all (1)";
+  assert_equal [1;2;3;1] (remove_all [1;2;3;1] 5 ) ~msg:"test_remove_all (2)";
+  assert_equal [true] (remove_all [true; false; false] false ) ~msg:"test_remove_all (3)";
+  assert_equal [] (remove_all [] 42 ) ~msg:"test_remove_all (4)"
+
+let test_index_of _ =
+   assert_equal 0 (index_of [1;2;3;1] 1) ~msg:"test_index_of (1)";
+   assert_equal 3 (index_of [4;2;3;1] 1) ~msg:"test_index_of (2)";
+   assert_equal 1 (index_of [true; false; false] false) ~msg:"test_index_of (3)";
+   assert_equal (-1) (index_of [] 42) ~msg:"test_index_of (4)"
 
   let suite =
     "public" >::: [
       "abs" >:: test_abs;
+      "remove_all" >:: test_remove_all;
+      "index_of" >:: test_index_of;
       ]
   
   let _ = run_test_tt_main suite
